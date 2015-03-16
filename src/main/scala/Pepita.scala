cca5b05249d5730757b54a81ae03a803c4c9399d
Federico Aloi <federico.aloi@gmail.com>
2015-03-16 00:32:18 -0300

    Creada clase Golondrina

--- src/main/scala/Pepita.scala
+++ src/main/scala/Pepita.scala
@@ -1,3 +1,10 @@
+class Golondrina(energia: Int) {
+  override def toString: String = "Yo soy una golondrina con " + energia + " de energia"
+}
+
 object Main extends App {
   println("Esto sale por la consola")
+
+  val pepita = new Golondrina(150)
+  println(pepita)
 }
